b9b085d36ae8863fc8fb66705d32e8bdbd759dfd
Kieran Chafin <kchafin@arizona.edu>
2025-04-17 04:14:52 -0700

    Started working on a comprehensive view for instructor

--- src/main/model/Instructor.java
+++ src/main/model/Instructor.java
@@ -5,7 +5,16 @@ import java.util.ArrayList;
 public class Instructor extends User {
     private ArrayList<Course> coursesManaged;
 
-    public Instructor(String username, String password, String name, boolean isHashed) {
-        super(username, password, name, isHashed);
+    public Instructor(String username, String password, String email, boolean isHashed) {
+        super(username, password, email, isHashed);
+    }
+
+    public Course getCoursesForDay(Days day) {
+        for (Course course : coursesManaged) {
+            if (course.getDays().contains(day)) {
+                return course;
+            }
+        }
+        return null; // No class found for the given day
     }
 }
