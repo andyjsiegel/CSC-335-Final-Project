f34bc17cdc141dc0934854cd824564c86449243d
andyjsiegel <andyjsiegel1@gmail.com>
2025-04-22 16:42:53 -0700

    Change studentList to use StudentList class instead of ArrayList, created setDefaultCategories method, changed method signatures to use StudentList, created method getAddAssignmentPanel (returns the panel to add an assignment) and fixed a typo

--- src/main/model/Course.java
+++ src/main/model/Course.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 
 import javax.swing.*;
 import java.awt.*;
+import main.view.NumberField;
 
 public class Course {
     private String code;
@@ -14,7 +15,7 @@ public class Course {
     private String description;
     private Instructor instructor;
     private ArrayList<Days> daysOfWeek;
-    private ArrayList<Student> studentList;
+    private StudentList studentList;
 
     private CourseAssignments assignments;
     private HashMap<String, Category> categories;
@@ -31,7 +32,7 @@ public class Course {
         this.instructor = instructor;
         this.daysOfWeek = new ArrayList<Days>();
         this.daysOfWeek = daysOfWeek;
-    	this.studentList = new ArrayList<Student>();
+    	this.studentList = new StudentList();
     	this.categories = new HashMap<String, Category>();
         this.assignments = new CourseAssignments();
     }
@@ -43,15 +44,23 @@ public class Course {
         this.description = other.description;
         this.instructor = new Instructor(other.instructor);
         this.daysOfWeek = new ArrayList<Days>(other.daysOfWeek);
-        //this.studentList = new StudentList(other.studentList);
+        this.studentList = new StudentList(other.studentList);
         //this.assignments = new CourseAssignments(other.assignments);
+    }
+
+    public void setDefaultCategories() {
+        // new Category(name, points, expPointsPerAssignment)
+        categories.put("Homeworks", new Category("Homeworks", 250, 25));
+        categories.put("Projects", new Category("Projects", 250, 50));
+        categories.put("Midterm Exam", new Category("Midterm Exam", 250, 250));
+        categories.put("Final Exam", new Category("Final Exam", 250, 250));
     }
 
     public CourseAssignments getAssignments() {
         return assignments;
     }
     
-    public ArrayList<Student> getStudents() {
+    public StudentList getStudents() {
         return studentList;
     }
 
@@ -64,11 +73,11 @@ public class Course {
     }
     
     public void addAssignment(Assignment assignment) {
-    	this.assignments.addAssignment(assignment);
+    	this.assignments.add(assignment);
     }
     
     public void removeAssignment(Assignment assignment) {
-    	this.assignments.removeAssignment(assignment);
+    	this.assignments.remove(assignment);
     }
     
     public void addDays(ArrayList<Days> daysOfWeek) {
@@ -96,6 +105,62 @@ public class Course {
 
     public ArrayList<Days> getDays() {
         return daysOfWeek;
+    }
+
+    public JPanel getAssignmentAddPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setBorder(BorderFactory.createTitledBorder("Add Assignment"));
+        
+        JLabel titleLabel = new JLabel("Title:");
+        panel.add(titleLabel);
+        JTextField titleField = new JTextField();
+        titleField.setColumns(20);
+        panel.add(titleField);
+
+        JLabel descriptionLabel = new JLabel("Description:");
+        panel.add(descriptionLabel);
+        JTextArea descriptionField = new JTextArea();
+        descriptionField.setColumns(20);
+        descriptionField.setRows(5);
+        descriptionField.setLineWrap(true);
+        descriptionField.setWrapStyleWord(true);
+        panel.add(descriptionField);
+
+        JLabel categoryLabel = new JLabel("Category:");
+        panel.add(categoryLabel);
+        JComboBox<String> categoryField = new JComboBox<String>(categories.keySet().toArray(new String[0]));
+        categoryField.setSelectedIndex(0);
+        
+        panel.add(categoryField);
+
+        JLabel maxPointsLabel = new JLabel("Max Points:");
+        panel.add(maxPointsLabel);
+        NumberField maxPointsField = new NumberField();
+        maxPointsField.setColumns(5);
+        maxPointsField.setText(categories.get(categoryField.getSelectedItem().toString()).getExpectedPoints() + "");
+        panel.add(maxPointsField);
+
+        categoryField.addActionListener(_ -> {
+            String selectedCategory = (String) categoryField.getSelectedItem();
+            maxPointsField.setText(categories.get(selectedCategory).getExpectedPoints() + "");
+        });
+
+        JButton addButton = new JButton("Add");
+        addButton.addActionListener(e -> {
+            String title = titleField.getText();
+            String description = descriptionField.getText();
+            String category = categoryField.getSelectedItem().toString();
+            int maxPoints = Integer.parseInt(maxPointsField.getText());
+            Assignment assignment = new Assignment(title, description, maxPoints);
+            categories.get(category).addAssignment(assignment);
+            this.addAssignment(assignment);
+            titleField.setText("");
+            descriptionField.setText("");
+            categoryField.setSelectedIndex(0);
+        });
+        panel.add(addButton);
+        return panel;
     }
     
     public JTabbedPane getCourseView() {
@@ -132,7 +197,7 @@ public class Course {
         panel.add(instructorLabel, gbc);
     
         //for (Instructor instructor : instructors) {
-            JLabel instructorName = new JLabel(instructor.getUsername());
+            JLabel instructorName = new JLabel(instructor.getFullName());
             gbc.gridx = 1;
             gbc.gridy = 2;
             gbc.gridwidth = 1;
@@ -175,9 +240,16 @@ public class Course {
     
         JPanel assignmentsPanel = new JPanel();
         assignmentsPanel.setLayout(new BoxLayout(assignmentsPanel, BoxLayout.Y_AXIS));
-        ArrayList<String> categorieTitles = new ArrayList<String>(categories.keySet());
-        Collections.sort(categorieTitles);
-        for (String category : categorieTitles) {
+        
+        JButton addAssignmentButton = new JButton("Add Assignment");
+        addAssignmentButton.addActionListener(_ -> {
+            JOptionPane.showMessageDialog(null, this.getAssignmentAddPanel(), this.getCode(), JOptionPane.PLAIN_MESSAGE);
+        });
+        assignmentsPanel.add(addAssignmentButton);
+
+        ArrayList<String> categoryTitles = new ArrayList<String>(categories.keySet());
+        Collections.sort(categoryTitles);
+        for (String category : categoryTitles) {
             ArrayList<Assignment> assignments = categories.get(category).getAssignments();
 
             // Header button for collapsible section
@@ -224,10 +296,20 @@ public class Course {
             assignmentsPanel.add(assignmentsListPanel);
         }
 
+        JPanel classListPanel = new JPanel();
+        classListPanel.setLayout(new BoxLayout(classListPanel, BoxLayout.Y_AXIS));
+        if(studentList.isEmpty()) {
+            classListPanel.add(new JLabel("No students enrolled."));
+        }
+        for(Student student : studentList) {
+            classListPanel.add(new JLabel(student.getFirstName() + " " + student.getLastName()));
+        }
+
         // Wrap assignmentsPanel in a JScrollPane in case of many assignments/categories
         JScrollPane scrollPane = new JScrollPane(assignmentsPanel);
         scrollPane.setBorder(BorderFactory.createEmptyBorder());
         tabbedPane.addTab("Assignments", scrollPane);
+        tabbedPane.addTab("Classlist", new JScrollPane(classListPanel));
 
         return tabbedPane;
     }
